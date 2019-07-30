c527ed7528d92b0ba01f43e9dae8733ca90490cc
keita <ba0503naky@gmail.com>
2019-07-30 15:48:52 +0900

    first

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+const config = {
+  apiKey: "AIzaSyBsN3LGiI_8vez2-ydkHi5e68CDP63O9bA",
+  authDomain: "sanzoku-camp.firebaseapp.com",
+  databaseURL: "https://sanzoku-camp.firebaseio.com",
+  projectId: "sanzoku-camp",
+  storageBucket: "",
+  messagingSenderId: "712339712985",
+  appId: "1:712339712985:web:882b421f29395843"
+};
+
+firebase.initializeApp(config);
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
